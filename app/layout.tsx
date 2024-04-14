efd1769897cbb9f699f3f458b0acb6db5059c379
Prateek Gupta <90403254+guptaPrateek10@users.noreply.github.com>
2024-04-14 13:45:16 +0530

    font and metadata integrated & images added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,44 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import React from "react";
+import "./globals.css";
+import { Metadata } from "next";
+import { Inter, Space_Grotesk } from "next/font/google";
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-space-grotesk",
+});
+export const metaData: Metadata = {
+  title: "Dev OverFlow",
+  description: "A place for developers to ask questions and find answers",
+  icons: {
+    icon: "assets/images/site-logo.svg",
+  },
+};
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: "primary-gradient",
+          footerActionLink: "primary-text-gradient hover:text-primary-500",
+        },
+      }}
+    >
       <html lang="en">
-        <body>{children}</body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+          <h1 className="h1-bold">This is new text</h1>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
